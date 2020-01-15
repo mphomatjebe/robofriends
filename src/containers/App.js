dc19e426e1a2acc5cdc4e0c334ae4921f83281bc
mphomatjebe <samuel.matjebe@gmail.com>
2020-01-15 14:11:45 +0200

    pheew! done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,51 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './app.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import {setSearchField, requestRobots} from '../actions';
 
-
-class App extends React.Component {
-constructor(){
-	super()
-	this.state = {
-	robots: [],
-	searchfield: ''
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
-componentDidMount(){
-	fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-		return response.json();
-	}).then(users=>{
-		this.setState({robots: users});
-	})
-	
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}	
 }
 
-onSearchChange = (event) =>{
-	this.setState({searchfield: event.target.value})
-	
+
+class App extends React.Component {
+
+
+componentDidMount(){
+	this.props.onRequestRobots();
 }
+
 
 	render(){
 
-		const {robots, searchfield}= this.state;
+		const {searchField, onSearchChange, robots, isPending} = this.props;
 		const filteredRobots = robots.filter(robot =>{
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	})
 
-		return !robots.length ?
+		return isPending ?
 		<h1>Loading</h1>:
 	
 		(
 		<div className='tc'>
 		<h1 className='f1'>RoboFriends</h1>
-		< SearchBox searchChange={this.onSearchChange}/>
+		< SearchBox searchChange={onSearchChange}/>
 
 		<Scroll>
 			<ErrorBoundry>
@@ -58,4 +61,4 @@ onSearchChange = (event) =>{
 	
 }
 
-export default App;
+export default (connect(mapStateToProps, mapDispatchToProps))(App);
